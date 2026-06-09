dd618702f4b24f64e4d6a05d57b5ea9a63255f97
Jeevithr75 <jeevithr6301@gmail.com>
2026-06-10 00:06:13 +0530

    Added line drawing feature

--- main.c
+++ main.c
@@ -5,22 +5,22 @@
 
 char canvas[ROWS][COLS];
 
-void initializeCanvas()
+void initializeCanvas(void)
 {
-    for(int i = 0; i < ROWS; i++)
+    for (int i = 0; i < ROWS; i++)
     {
-        for(int j = 0; j < COLS; j++)
+        for (int j = 0; j < COLS; j++)
         {
             canvas[i][j] = '_';
         }
     }
 }
 
-void displayCanvas()
+void displayCanvas(void)
 {
-    for(int i = 0; i < ROWS; i++)
+    for (int i = 0; i < ROWS; i++)
     {
-        for(int j = 0; j < COLS; j++)
+        for (int j = 0; j < COLS; j++)
         {
             printf("%c ", canvas[i][j]);
         }
@@ -28,11 +28,46 @@ void displayCanvas()
     }
 }
 
-int main()
+void drawRectangle(int row, int col, int height, int width)
 {
+    int endRow = row + height;
+    int endCol = col + width;
+
+    if (row < 0) row = 0;
+    if (col < 0) col = 0;
+    if (endRow > ROWS) endRow = ROWS;
+    if (endCol > COLS) endCol = COLS;
+
+    for (int i = row; i < endRow; i++)
+    {
+        for (int j = col; j < endCol; j++)
+        {
+            canvas[i][j] = '*';
+        }
+    }
+}
+
+int main(void)
+{
+    int row, col, height, width;
+
     initializeCanvas();
 
-    printf("Initial Canvas:\n\n");
+    printf("Enter starting row: ");
+    scanf("%d", &row);
+
+    printf("Enter starting column: ");
+    scanf("%d", &col);
+
+    printf("Enter height of rectangle: ");
+    scanf("%d", &height);
+
+    printf("Enter width of rectangle: ");
+    scanf("%d", &width);
+
+    drawRectangle(row, col, height, width);
+
+    printf("\nCanvas after drawing rectangle:\n\n");
     displayCanvas();
 
     return 0;
